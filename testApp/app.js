05b3ec5f56d1a89d860e486678048061b5880269
Jake Robbins <jrobbinsd@gmail.com>
2019-05-14 20:24:00 -0600

    testing routes

--- testApp/app.js
+++ testApp/app.js
@@ -15,6 +15,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/test', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
